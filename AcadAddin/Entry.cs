fea3e9e0c3181c868c81ffcfb11e124fb8d5badb
Madhukar Moogala <madhukar.moogala@autodesk.com>
2024-12-19 11:00:53 +0530

    Add project files.

---
+++ AcadAddin/Entry.cs
@@ -0,0 +1,68 @@
+﻿using Autodesk.AutoCAD.Interop;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+
+namespace AcadAddin
+{
+    class Activation
+    {
+        /// <summary>
+        /// Managed definition of CoClass
+        /// </summary>
+        [ComImport]
+        [CoClass(typeof(ServerClass))]
+        [Guid("BA9AC84B-C7FC-41CF-8B2F-1764EB773D4B")] // By TlbImp convention, set this to the GUID of the parent interface
+        internal interface Server : IServer
+        {
+        }
+
+        /// <summary>
+        /// Managed activation for CoClass
+        /// </summary>
+        [ComImport]
+        [Guid("DB1797F5-7198-4411-8563-D05F4E904956")]
+        internal class ServerClass
+        {
+        }
+    }
+    public class Entry
+    {
+        
+
+        [CommandMethod("RunDLL")]
+        public void RunDLL()
+        {
+            var doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            if (doc == null)
+                return;
+            var editor = doc.Editor;
+            IAcadApplication acadObj = Autodesk.AutoCAD.ApplicationServices.Application.AcadApplication as IAcadApplication;
+            double pi = 0;
+            if (acadObj != null)
+            {
+                try
+                {
+                    IServer serverObj = acadObj.GetInterfaceObject("COMServer.Server") as IServer;
+
+
+                    if (serverObj != null)
+                    {
+                       pi = serverObj.ComputePi();
+                       editor.WriteMessage("\n" + pi);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    editor.WriteMessage("\n" + ex.Message);
+                    var server = new Activation.Server();
+                    pi = server.ComputePi();
+                    editor.WriteMessage("\n" + pi);
+
+                }
+            }
+        }
+    }
+}
---
+++ COMServer/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+[assembly: Guid("A8DAD545-A841-4EE4-B4DA-AAAC2FDDD305")]
+[assembly: TypeLibVersion(1, 0)]
+namespace COMServer
+{
+    [ComVisible(true)]
+    [Guid(ContractGuids.ServerClass)]
+    public class Server : IServer
+    {
+        double IServer.ComputePi()
+        {
+            double sum = 0.0;
+            int sign = 1;
+            for (int i = 0; i < 1024; ++i)
+            {
+                sum += sign / (2.0 * i + 1.0);
+                sign *= -1;
+            }
+
+            return 4.0 * sum;
+        }
+
+        void IDispatch.GetIDsOfNames(ref Guid riid, ref nint rgszNames, int cNames, int lcid, ref nint rgDispId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDispatch.GetTypeInfo(int iTInfo, int lcid, out nint info)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDispatch.GetTypeInfoCount(out int pctinfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDispatch.Invoke(int dispIdMember, ref Guid riid, int lcid, short wFlags, ref DISPPARAMS pDispParams, out object pVarResult, ref EXCEPINFO pExcepInfo, out int puArgErr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
